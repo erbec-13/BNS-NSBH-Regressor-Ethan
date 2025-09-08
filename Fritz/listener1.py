55406d0084d290042e1457f2134d89f630897117
Ethan Erb <erbec13@gmail.com>
2025-09-08 09:35:27 -0400

    Update listener1 so that it matches listener1backup

--- Fritz/listener1.py
+++ Fritz/listener1.py
@@ -18,6 +18,7 @@ from utils import (
 from joblib import load
 from Model import build_lstm_model
 import json
+from tensorflow.keras.models import load_model
 #import traceback
 #import faulthandler
 #faulthandler.enable()
@@ -46,13 +47,7 @@ if not GCN_CLIENT_ID or not GCN_CLIENT_SECRET:
         "GCN_CLIENT_ID and GCN_CLIENT_SECRET must be set as environment variables"
     )
 
-config = {
-    "group.id": GCN_GROUP_ID,
-    "auto.offset.reset": "earliest",
-    "enable.auto.commit": False,
-    "socket.keepalive.enable": True,
-    "broker.address.family": "v4",  # Force IPv4 only
-}
+config = {'group.id': 'bnsAndnsbhLCforslack', 'auto.offset.reset': 'earliest', 'enable.auto.commit': False}
 consumer = Consumer(
     config=config,
     client_id=GCN_CLIENT_ID,
@@ -69,10 +64,10 @@ consumer.subscribe(
 
 client = GraceDb()
 
-superevent_id = "S230627c"
+superevent_id = "S250818k"
 
 # Choose the VOEvent file you want
-filename = "S230627c-4-Update.xml"
+filename = "S250818k-4-Update.xml"
 
 # Download the file content
 response = client.files(superevent_id, filename)
@@ -91,69 +86,45 @@ content = voevent.read()
 base_dir = os.path.dirname(os.path.abspath(__file__))
 print(base_dir)
 print("Loading model and scalers...")
-target_scaler = load(f"{base_dir}/target_scaler_PAstro.joblib")
-feature_scaler = load(f"{base_dir}/feature_scaler_PAstro.joblib")
+target_scaler = load(f"{base_dir}/target_scaler_O4.joblib")
+feature_scaler = load(f"{base_dir}/feature_scaler_O4.joblib")
 print("Building LSTM model once...")
 num_time_points = 30
-input_shape = (num_time_points, feature_scaler.transform(np.zeros((1, 8))).shape[1])
-model = load(f"{base_dir}/LSTMpredLC__PAstro.joblib")
+input_shape = (num_time_points, feature_scaler.transform(np.zeros((1, 6))).shape[1])
+#model = build_lstm_model(input_shape=input_shape)
+#model = load(f"{base_dir}/LSTMpredLC__PAstro.joblib")
+model = load_model(
+    f"{base_dir}/LSTM_model_production.h5",
+    compile=False  # Ignore optimizer, loss, and metrics
+)
 print(feature_scaler.feature_names_in_)
 
 
 counter = 0
-while counter<2:
+while True:
     print(counter)
     try:
         for message in consumer.consume():
             value = message.value()
+            print('a')
             parsed = parse_gcn(content)
+            print('b')
             params = get_params(parsed)
-            (
-                superevent_id,
-                event_page,
-                alert_type,
-                group,
-                prob_bbh,
-                prob_bns,
-                prob_nsbh,
-                far_format,
-                distmean,
-                area_90,
-                has_ns,
-                has_remnant,
-                has_mass_gap,
-                significant,
-                prob_ter,
-                skymap,
-                PAstro,
-                time,
-                skymap_type,
-                longitude,
-                latitude
-            ) = params
+            print('c')
+            superevent_id, event_page, alert_type, group, prob_bbh, prob_bns, prob_nsbh, far_format, distmean, area_90, longitude, latitude, has_ns, has_remnant, has_mass_gap, significant, prob_ter, skymap, PAstro, time = params
 
             print(superevent_id + " " + alert_type)
             counter+=1
             if alert_type != "RETRACTION" and distmean != "error": #superevent_id == "S250818k" and 
                 print(f"Processing {superevent_id} ({alert_type})")
                 
-                X = pd.DataFrame(
-                    np.vstack(
-                        (
-                            area_90,
-                            distmean,
-                            has_ns,
-                            has_remnant,
-                            has_mass_gap,
-                            PAstro,
-                        )
-                    ).T,
-                    columns=["area(90)", "distance", "HasNS", "HasRemnant", "HasMassGap", "PAstro"]
-                )
+                X = np.vstack((area_90, distmean, has_ns, has_remnant, has_mass_gap, PAstro)).T
+
+                print(X)
 
                 # Time array
                 t_min = 0.1
-                t_max = 14.0
+                t_max = 6.0
                 dt = 0.2
                 time_single = np.linspace(t_min, t_max, num_time_points)
                 filter_order = 3
@@ -162,29 +133,23 @@ while counter<2:
                 X_new = feature_scaler.transform(X)
 
                 # Reshape X data for LSTM input based on the model's input shape
-                X_new_reshaped = np.tile(X_new, (1, num_time_points)).reshape((X_new.shape[0], num_time_points, X_new.shape[1]))
+                X_new_reshaped = X_new.reshape((X_new.shape[0], 1, X_new.shape[1]))
                 #X_new_reshaped = X_new.reshape((X_new.shape[0], 90, X_new.shape[1]))
 
                 #X_tiled = np.tile(X_new, (1, num_time_points)).reshape((X_new.shape[0], num_time_points, X_new.shape[1]))
 
 
-                mean_preds_new, uncertainty_new = predict_with_uncertainty(
-                    model, X_new_reshaped, n_iter=1000
-                )
+                n_mc_samples = 1000
+                mean_preds_new, uncertainty_new = predict_with_uncertainty(model, X_new_reshaped, n_iter=n_mc_samples)
 
                 # Reshape the mean predictions to match the shape used during scaling (num_samples, num_time_points * num_filters)
-                mean_preds_flat = mean_preds_new.reshape(
-                    mean_preds_new.shape[0], num_time_points * 3
-                )
+                mean_preds_flat = mean_preds_new.reshape(mean_preds_new.shape[0], num_time_points * 3)
+
                 # Invert the standardization for the mean predictions
-                mean_preds_inverted = target_scaler.inverse_transform(
-                    mean_preds_flat
-                ).reshape(mean_preds_new.shape[0], num_time_points, 3)
+                mean_preds_inverted = target_scaler.inverse_transform(mean_preds_flat).reshape(mean_preds_new.shape[0], num_time_points, 3)
 
                 # Reshape uncertainty to match mean_preds_inverted shape
-                uncertainty_reshaped = uncertainty_new.reshape(
-                    uncertainty_new.shape[0], num_time_points, 3
-                )
+                uncertainty_reshaped = uncertainty_new.reshape(uncertainty_new.shape[0], num_time_points, 3)
 
                 event_data = {"time_single": time_single.tolist(), "mean_preds_inverted": mean_preds_inverted.tolist(), "uncertainty_reshaped": uncertainty_reshaped.tolist(), "time": time, "alert_type": alert_type}
                 
@@ -206,8 +171,7 @@ while counter<2:
                     mean_preds_inverted,
                     uncertainty_reshaped,
                     superevent_id,
-                    time,
-                    alert_type,
+                    time
                 )
             else:
                 counter-=1
